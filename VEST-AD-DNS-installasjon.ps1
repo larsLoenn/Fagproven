16ee1eaf2a3e991820453a4b7a41ebbc6df41248
Lars Loenn <106143918+larsLoenn@users.noreply.github.com>
2025-08-19 10:45:43 -0700

    Update VEST-AD-DNS-installasjon.ps1

--- VEST-AD-DNS-installasjon.ps1
+++ VEST-AD-DNS-installasjon.ps1
@@ -22,17 +22,22 @@ Install-ADDSForest `
 # Windows PowerShell script for AD DS Deployment
 #
 
+#
+# Windows PowerShell script for AD DS Deployment
+#
+
 Import-Module ADDSDeployment
 Install-ADDSDomainController `
 -NoGlobalCatalog:$false `
 -CreateDnsDelegation:$false `
+-Credential (Get-Credential) `
 -CriticalReplicationOnly:$false `
 -DatabasePath "C:\Windows\NTDS" `
--DomainName "OEKOMMUNE.ad" `
+-DomainName "vestkommune.ad" `
 -InstallDns:$true `
 -LogPath "C:\Windows\NTDS" `
 -NoRebootOnCompletion:$false `
--ReplicationSourceDC "OEK-DC01.OEKOMMUNE.ad" `
+-ReplicationSourceDC "DC01.vestkommune.ad" `
 -SiteName "Default-First-Site-Name" `
 -SysvolPath "C:\Windows\SYSVOL" `
 -Force:$true
